9dee4b3f7d3eb320545d71baecbd29529e746e1c
Caleb Sacks <16855387+clabe45@users.noreply.github.com>
2021-12-14 01:48:13 -0500

    optimize get_renamed_variables

--- filter/filter/java.py
+++ filter/filter/java.py
@@ -100,9 +100,29 @@ def filter_ast_node(node: Node, t: type) -> List[Union[Node, str]]:
 	return _filter_ast_node(node, t)
 
 
-def _have_same_references(var1: str, var2: str, a_refs: List[Tuple], b_refs: List[Tuple], matches: List[Tuple]) -> bool:
-	ar = [(path, ref) for path, ref in a_refs if ref.member == var1]
-	br = [(path, ref) for path, ref in b_refs if ref.member == var2]
+def chunk_ast_nodes_by_path(nodes: List[Tuple[List, Node]]) -> List[Tuple[List, List[Node]]]:
+	chunks = []
+	curr_path = None
+	curr_chunk = None
+	for path, node in nodes:
+		if path != curr_path:
+			if curr_path is not None:
+				chunks.append((curr_path, curr_chunk))
+
+			curr_path = path
+			curr_chunk = []
+
+		curr_chunk.append(node)
+
+	if curr_path is not None:
+		chunks.append((curr_path, curr_chunk))
+
+	return chunks
+
+
+def _have_same_references(a_var: str, b_var: str, a_refs: List[Tuple], b_refs: List[Tuple], matches: List[Tuple]) -> bool:
+	ar = [(path, ref) for path, refs in a_refs for ref in refs if ref.member == a_var]
+	br = [(path, ref) for path, refs in b_refs for ref in refs if ref.member == b_var]
 	if len(ar) != len(br):
 		return False
 
@@ -123,14 +143,14 @@ def get_renamed_variables(source_code: str, target_code: str) -> Dict[str, str]:
 	except javalang.parser.JavaSyntaxError as e:
 		return None
 
-	source_declarations = list(filter_ast_node(source_tree, VariableDeclaration))
-	target_declarations = list(filter_ast_node(target_tree, VariableDeclaration))
+	source_declarations = chunk_ast_nodes_by_path(filter_ast_node(source_tree, VariableDeclaration))
+	target_declarations = chunk_ast_nodes_by_path(filter_ast_node(target_tree, VariableDeclaration))
 
-	source_var_names = [declarator.name for path, declaration in source_declarations for declarator in declaration.declarators]
-	target_var_names = [declarator.name for path, declaration in target_declarations for declarator in declaration.declarators]
+	source_var_names = [declarator.name for path, declarations in source_declarations for declaration in declarations for declarator in declaration.declarators]
+	target_var_names = [declarator.name for path, declarations in target_declarations for declaration in declarations for declarator in declaration.declarators]
 
-	source_references = list(filter_ast_node(source_tree, MemberReference))
-	target_references = list(filter_ast_node(target_tree, MemberReference))
+	source_references = chunk_ast_nodes_by_path(filter_ast_node(source_tree, MemberReference))
+	target_references = chunk_ast_nodes_by_path(filter_ast_node(target_tree, MemberReference))
 
 	renamed_var_names = []
 	matches = []
@@ -142,91 +162,101 @@ def get_renamed_variables(source_code: str, target_code: str) -> Dict[str, str]:
 	# - They must have be in the same semantic section of the file
 	# - They must have the same references and assignments
 
-	for source_dec_path, source_dec_node in source_declarations:
-		for target_dec_path, target_dec_node in target_declarations:
+	# Iterate over every pair of chunks, including the pair of the same chunk.
+	for i in range(len(source_declarations)):
+		source_decn_path, source_decn_nodes = source_declarations[i]
+		for j in range(i, len(target_declarations)):
+			target_decn_path, target_decn_nodes = target_declarations[j]
+
 			# 1. Make sure paths would be equal if source_node (a) and
 			#	 target_node (b) were the same.
 
 			# 1a. Must be in the same part of the file
 			# First get all previously discovered renamed variables
-			prev_matches_here = [(old, new, attrs) for path, v in matches for old, new, attrs in v if path == target_dec_path]
+			prev_matches_here = [(old, new, attrs) for path, v in matches for old, new, attrs in v if path in (source_decn_path, target_decn_path)]
+
 			# Now make sure the paths are equal (ignoring the previously found
 			# renamed variables and the declarations themselves)
-			if not ast_paths_equal(source_dec_path, target_dec_path, matches=prev_matches_here + [(source_dec_node, target_dec_node, ['type', 'declarators'])]):
+			if not ast_paths_equal(source_decn_path, target_decn_path, matches=prev_matches_here):
 				continue
 
-			for source_declarator in source_dec_node.declarators:
-				# 1b. (Must have different names)
-				if source_declarator.name in target_var_names:
-					continue
-
-				for target_declarator in target_dec_node.declarators:
-					# 1c. All attributes except the name should be equal
-					declarator_match = (source_declarator, target_declarator, 'name')
-					if not ast_nodes_equal(source_declarator, target_declarator, recursive=True, matches=prev_matches_here + [declarator_match]):
+			for k in range(len(source_decn_nodes)):
+				source_decn_node = source_decn_nodes[k]
+				for l in range(len(source_decn_node.declarators)):
+					source_declarator = source_decn_node.declarators[l]
+					# 1b. Must have different names
+					if source_declarator.name in target_var_names:
 						continue
 
-					# 1c. Must have the same references
-					if not _have_same_references(source_declarator.name, target_declarator.name, source_references, target_references, prev_matches_here):
-						continue
+					sr = [(path, ref) for path, refs in source_references for ref in refs if ref.member == source_declarator.name]
 
-					sr = [(path, ref) for path, ref in source_references if ref.member == source_declarator.name]
-					tr = [(path, ref) for path, ref in target_references if ref.member == target_declarator.name]
+					for m in range(k, len(target_decn_nodes)):
+						target_decn_node = target_decn_nodes[m]
+						for n in range(len(target_decn_node.declarators)):
+							target_declarator = target_decn_node.declarators[n]
+							# 1c. All attributes except the name should be equal
+							declarator_match = (source_declarator, target_declarator, 'name')
+							if not ast_nodes_equal(source_declarator, target_declarator, recursive=True, matches=prev_matches_here + [declarator_match]):
+								continue
 
-					for i in range(len(sr)):
-						source_ref_path, source_ref_node = sr[i]
-						target_ref_path, target_ref_node = tr[i]
-						ref_match = (source_ref_node, target_ref_node, 'name')
+							# 1d. Must have the same references
+							if not _have_same_references(source_declarator.name, target_declarator.name, source_references, target_references, prev_matches_here):
+								continue
 
-						for path, mappings in matches:
-							if path == target_ref_path:
+							tr = [(path, ref) for path, refs in target_references for ref in refs if ref.member == target_declarator.name]
+
+							# (sr and tr have the same length, so we can do this safely)
+							for i in range(len(sr)):
+								source_ref_path, source_ref_node = sr[i]
+								target_ref_path, target_ref_node = tr[i]
+								ref_match = (source_ref_node, target_ref_node, 'name')
+
+								for path, mappings in matches:
+									if path == target_ref_path:
+										mappings.append(ref_match)
+
+								else:
+									matches.append((target_ref_path, [ref_match]))
+
+							# 1d. (The function cannot return multiple mappings
+							#     containing the same variable at the same path)
+							other_renames_at_path = False
+							never_used = True
+							for path, mappings in renamed_var_names:
+								if path != target_decn_path:
+									continue
+
 								for old, new in mappings:
+									if old == source_declarator.name or new == target_declarator.name:
+										never_used = False
+										break
+
+								if not never_used:
 									break
 
-								mappings.append(ref_match)
-
-						else:
-							matches.append((target_ref_path, [ref_match]))
-
-					# 1d. (The function cannot return multiple mappings
-					#     containing the same variable at the same path)
-					other_renames_at_path = False
-					never_used = True
-					for path, mappings in renamed_var_names:
-						if path != target_dec_path:
-							continue
-
-						for old, new in mappings:
-							if old == source_declarator.name or new == target_declarator.name:
-								never_used = False
+								mappings.append((source_declarator.name, target_declarator.name))
+								other_renames_at_path = True
 								break
 
-						if not never_used:
-							break
+							if not never_used:
+								continue
 
-						mappings.append((source_declarator.name, target_declarator.name))
-						other_renames_at_path = True
-						break
+							if not other_renames_at_path:
+								mappings = [(source_declarator.name, target_declarator.name)]
+								renamed_var_names.append((target_decn_path, mappings))
 
-					if not never_used:
-						continue
+							for path, v in matches:
+								if path == target_decn_path:
+									for old, new, _ in v:
+										if old == source_declarator:
+											raise JavaAnalyzationError(f'duplicate declarator in source: {old}')
 
-					if not other_renames_at_path:
-						mappings = [(source_declarator.name, target_declarator.name)]
-						renamed_var_names.append((target_dec_path, mappings))
+										if new == target_declarator:
+											raise JavaAnalyzationError(f'duplicate declarator in target: {new}')
 
-					for path, v in matches:
-						if path == target_dec_path:
-							for old, new, _ in v:
-								if old == source_declarator:
-									raise JavaAnalyzationError(f'duplicate declarator in source: {old}')
+									v.append(declarator_match)
 
-								if new == target_declarator:
-									raise JavaAnalyzationError(f'duplicate declarator in target: {new}')
-
-							v.append(declarator_match)
-
-					else:
-						matches.append((target_dec_path, [declarator_match]))
+							else:
+								matches.append((target_decn_path, [declarator_match]))
 
 	return renamed_var_names
