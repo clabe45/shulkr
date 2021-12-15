d200bc3ef183185fa39c2a1beabec9eebf12e2a8
Caleb Sacks <16855387+clabe45@users.noreply.github.com>
2021-12-15 12:46:32 -0500

    rename -r to -p

--- filter/__init__.py
+++ filter/__init__.py
@@ -11,7 +11,7 @@ from filter.minecraft import generate_sources
 
 def parse_args() -> argparse.Namespace:
 	parser = argparse.ArgumentParser(prog='filter', description='Generate multiple versions of the Minecraft source code')
-	parser.add_argument('--repo', '-r', type=str, required=True, help='Path to the Minecraft repo')
+	parser.add_argument('--repo', '-p', type=str, required=True, help='Path to the Minecraft repo')
 	parser.add_argument('--undo-renamed-vars', '-u', dest='undo_renamed_vars', action='store_true', help='Revert local variables that were renamed in the new version')
 	parser.add_argument('version', nargs='+', type=str, help='List of mapping versions')
 	parser.set_defaults(undo_renamed_vars=False)
