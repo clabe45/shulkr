e5cbec14de658bc062972df28ad30b47503f333d
Caleb Sacks <16855387+clabe45@users.noreply.github.com>
2021-12-16 01:49:06 -0500

    move DecompilerMC into shulkr module
    So it can be packaged with build

--- shulkr/minecraft/source.py
+++ shulkr/minecraft/source.py
@@ -8,8 +8,11 @@ from .version import Version
 
 
 def generate_sources(source_repo: str, version: Version) -> None:
+	script_dir = os.path.dirname(__file__)
+	decompiler_dir = os.path.join(script_dir, '..', 'DecompilerMC')
+
 	# 1. Generate source code there
-	subprocess.run(['python3', 'main.py', '--mcv', str(version), '-c', '-f', '-q'], cwd='DecompilerMC')
+	subprocess.run(['python3', 'main.py', '--mcv', str(version), '-c', '-f', '-q'], cwd=decompiler_dir)
 
 	# 2. Move the generated source code to the target repo
 	dest_src_dir = os.path.join(source_repo, 'src')
@@ -17,6 +20,6 @@ def generate_sources(source_repo: str, version: Version) -> None:
 		rmtree(dest_src_dir)
 
 	move(
-		os.path.join('DecompilerMC', 'src'),
+		os.path.join(decompiler_dir, 'src'),
 		source_repo
 	)
