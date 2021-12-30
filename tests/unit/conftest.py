6e7b1c9464a6cbfd04530c441df75ed794a43202
Caleb Sacks <16855387+clabe45@users.noreply.github.com>
2021-12-30 14:13:49 -0500

    add fixture for mocking the versions manifest

--- tests/unit/conftest.py
+++ tests/unit/conftest.py
@@ -1,6 +1,31 @@
 import git
 import pytest
 
+from shulkr.minecraft.version import Version, load_manifest
+
+
+MANIFEST_DATA = {
+	'latest': {
+		'release': '1.0.0',
+		'snapshot': '1.0.0'
+	},
+	'versions': [
+		{
+			'type': 'release',
+			'id': '1.0.0'
+		},
+		{
+			'type': 'snapshot',
+			'id': 'abcdef'
+		}
+	]
+}
+
+class TestVersions:
+	def __init__(self, snapshot: Version, release: Version) -> None:
+		self.snapshot = snapshot
+		self.release = release
+
 
 @pytest.fixture(scope='function')
 def repo(mocker):
@@ -11,3 +36,12 @@ def repo(mocker):
 	mocker.patch.object(repo.index, 'commit')
 
 	return repo
+
+
+@pytest.fixture(scope='function')
+def versions():
+	load_manifest(MANIFEST_DATA, earliest_supported_version_id='abcdef')
+
+	snapshot = Version.of('abcdef')
+	release = Version.of('1.0.0')
+	return TestVersions(snapshot, release)
--- tests/unit/minecraft/test_version.py
+++ tests/unit/minecraft/test_version.py
@@ -1,91 +1,69 @@
 import pytest
 
-from shulkr.minecraft.version import Version, load_manifest
-
-MANIFEST_DATA = {
-	'latest': {
-		'release': '1.0.0',
-		'snapshot': '1.0.0'
-	},
-	'versions': [
-		{
-			'type': 'release',
-			'id': '1.0.0'
-		},
-		{
-			'type': 'snapshot',
-			'id': 'abcdef'
-		}
-	]
-}
-
-load_manifest(MANIFEST_DATA, earliest_supported_version_id='abcdef')
+from shulkr.minecraft.version import Version
 
 
 class TestVersion:
-	snapshot = Version.of('abcdef')
-	release = Version.of('1.0.0')
+	def test_next_of_snapshot_is_the_release(self, versions):
+		assert versions.snapshot.next == versions.release
 
-	def test_next_of_snapshot_is_the_release(self):
-		assert self.snapshot.next == self.release
+	def test_next_of_release_is_none(self, versions):
+		assert versions.release.next is None
 
-	def test_next_of_release_is_none(self):
-		assert self.release.next is None
+	def test_release_does_not_equal_snapshot(self, versions):
+		assert versions.release != versions.snapshot
 
-	def test_release_does_not_equal_snapshot(self):
-		assert self.release != self.snapshot
+	def test_snapshot_is_less_than_release(self, versions):
+		assert versions.snapshot < versions.release
 
-	def test_snapshot_is_less_than_release(self):
-		assert self.snapshot < self.release
+	def test_release_is_greater_than_snapshot(self, versions):
+		assert versions.release > versions.snapshot
 
-	def test_release_is_greater_than_snapshot(self):
-		assert self.release > self.snapshot
+	def test_snapshot_to_release_returns_list_containing_snapshot_and_release(self, versions):
+		assert versions.snapshot.to(versions.release) == [versions.snapshot, versions.release]
 
-	def test_snapshot_to_release_returns_list_containing_snapshot_and_release(self):
-		assert self.snapshot.to(self.release) == [self.snapshot, self.release]
+	def test_snapshot_to_snapshot_returns_list_containing_snapshot(self, versions):
+		assert versions.snapshot.to(versions.snapshot) == [versions.snapshot]
 
-	def test_snapshot_to_snapshot_returns_list_containing_snapshot(self):
-		assert self.snapshot.to(self.snapshot) == [self.snapshot]
+	def test_snapshot_to_none_returns_list_containing_snapshot_and_release(self, versions):
+		assert versions.snapshot.to(None) == [versions.snapshot, versions.release]
 
-	def test_snapshot_to_none_returns_list_containing_snapshot_and_release(self):
-		assert self.snapshot.to(None) == [self.snapshot, self.release]
+	def test_snapshot_to_release_excluding_snapshots_returns_list_containing_release(self, versions):
+		assert versions.snapshot.to(versions.release, snapshots=False) == [versions.release]
 
-	def test_snapshot_to_release_excluding_snapshots_returns_list_containing_release(self):
-		assert self.snapshot.to(self.release, snapshots=False) == [self.release]
-
-	def test_release_to_snapshot_raises_version_error(self):
+	def test_release_to_snapshot_raises_version_error(self, versions):
 		with pytest.raises(ValueError):
-			self.release.to(self.snapshot)
+			versions.release.to(versions.snapshot)
 
-	def test_pattern_with_no_range_operator_returns_list_containing_version(self):
-		assert Version.pattern('1.0.0') == [self.release]
+	def test_pattern_with_no_range_operator_returns_list_containing_version(self, versions):
+		assert Version.pattern('1.0.0') == [versions.release]
 
-	def test_pattern_with_two_dots_returns_list_containing_release(self):
-		assert Version.pattern('..') == [self.release]
+	def test_pattern_with_two_dots_returns_list_containing_release(self, versions):
+		assert Version.pattern('..') == [versions.release]
 
-	def test_pattern_with_three_dots_returns_list_containing_snapshot_and_release(self):
-		assert Version.pattern('...') == [self.snapshot, self.release]
+	def test_pattern_with_three_dots_returns_list_containing_snapshot_and_release(self, versions):
+		assert Version.pattern('...') == [versions.snapshot, versions.release]
 
-	def test_pattern_with_release_two_dots_returns_list_containing_release(self):
-		assert Version.pattern('1.0.0..') == [self.release]
+	def test_pattern_with_release_two_dots_returns_list_containing_release(self, versions):
+		assert Version.pattern('1.0.0..') == [versions.release]
 
-	def test_pattern_with_release_two_dots_release_returns_list_containing_release(self):
-		assert Version.pattern('1.0.0..1.0.0') == [self.release]
+	def test_pattern_with_release_two_dots_release_returns_list_containing_release(self, versions):
+		assert Version.pattern('1.0.0..1.0.0') == [versions.release]
 
-	def test_pattern_with_snapshot_three_dots_release_returns_list_containing_snapshot_and_release(self):
-		assert Version.pattern('abcdef...1.0.0') == [self.snapshot, self.release]
+	def test_pattern_with_snapshot_three_dots_release_returns_list_containing_snapshot_and_release(self, versions):
+		assert Version.pattern('abcdef...1.0.0') == [versions.snapshot, versions.release]
 
-	def test_patterns_with_empty_list_returns_empty_list(self):
+	def test_patterns_with_empty_list_returns_empty_list(self, versions):
 		assert Version.patterns([]) == []
 
-	def test_patterns_with_one_positive_id_returns_corresponding_version(self, mocker):
-		assert Version.patterns(['1.0.0']) == [self.release]
+	def test_patterns_with_one_positive_id_returns_corresponding_version(self, versions):
+		assert Version.patterns(['1.0.0']) == [versions.release]
 
-	def test_patterns_with_two_positive_identical_ids_returns_one_version(self, mocker):
-		assert Version.patterns(['1.0.0', '1.0.0']) == [self.release]
+	def test_patterns_with_two_positive_identical_ids_returns_one_version(self, versions):
+		assert Version.patterns(['1.0.0', '1.0.0']) == [versions.release]
 
-	def test_patterns_with_one_positive_id_and_the_same_negative_id_returns_empty_list(self, mocker):
+	def test_patterns_with_one_positive_id_and_the_same_negative_id_returns_empty_list(self, versions):
 		assert Version.patterns(['1.0.0', '-1.0.0']) == []
 
-	def test_patterns_with_one_positive_id_and_the_same_negative_id_and_the_same_positive_id_returns_one_version(self, mocker):
-		assert Version.patterns(['1.0.0', '-1.0.0', '1.0.0']) == [self.release]
+	def test_patterns_with_one_positive_id_and_the_same_negative_id_and_the_same_positive_id_returns_one_version(self, versions):
+		assert Version.patterns(['1.0.0', '-1.0.0', '1.0.0']) == [versions.release]
