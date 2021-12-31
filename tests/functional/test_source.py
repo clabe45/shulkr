ab4e776f34142fb2234d01887c3c43e4db5d3931
Caleb Sacks <16855387+clabe45@users.noreply.github.com>
2021-12-30 19:51:45 -0500

    ignore order when checking repo directory contents

--- tests/functional/test_source.py
+++ tests/functional/test_source.py
@@ -2,7 +2,7 @@ import os
 
 
 def test_repo_only_contains_git_client_and_server_directories(generate_1_18):
-	assert os.listdir(generate_1_18.repo_path) == ['.git', 'client', 'server']
+	assert set(os.listdir(generate_1_18.repo_path)) == set(['.git', 'client', 'server'])
 
 
 def test_server_and_client_src_directories_are_not_empty(generate_1_18):
