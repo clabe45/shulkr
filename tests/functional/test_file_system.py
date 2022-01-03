3d45b46b2ddd61778ecd3ad367d90b5a6a0c7512
Caleb Sacks <16855387+clabe45@users.noreply.github.com>
2022-01-02 21:04:47 -0500

    improve functional test's readability

--- tests/functional/test_file_system.py
+++ tests/functional/test_file_system.py
@@ -8,4 +8,4 @@ def test_repo_only_contains_git_client_and_server_directories(run):
 def test_server_and_client_src_directories_are_not_empty(run):
 	for env in ('client', 'server'):
 		src_dir = os.path.join(run.repo_path, env, 'src')
-		assert len(os.listdir(src_dir))
+		assert len(os.listdir(src_dir)) > 0
