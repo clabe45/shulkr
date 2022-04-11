7dac9f3a3a76333ea6ba1b980538d0a4489e8798
Caleb Sacks <16855387+clabe45@users.noreply.github.com>
2022-04-11 23:42:38 +0000

    test: use temp directory for repo in tests
    For some reason, if the repo was in the same directory as the functional
    tests, trying to remove it resulted in an error in GitHub Actions.

--- tests/functional/conftest.py
+++ tests/functional/conftest.py
@@ -1,6 +1,7 @@
 import os
 import shutil
 import subprocess
+import tempfile
 from typing import List
 
 import pytest
@@ -35,28 +36,24 @@ def create_command(
 
 
 def _run(versions: List[str], undo_renamed_vars: bool) -> None:
-	script_dir = os.path.dirname(__file__)
-	repo_path = os.path.join(script_dir, 'repo')
+	with tempfile.TemporaryDirectory(prefix='shulkr') as repo_path:
+		try:
+			command = create_command(versions, repo_path, undo_renamed_vars)
+			p = subprocess.run(command, stderr=subprocess.PIPE)
 
-	try:
-		command = create_command(versions, repo_path, undo_renamed_vars)
-		p = subprocess.run(command, stderr=subprocess.PIPE)
+		except KeyboardInterrupt as e:
+			if os.path.exists(repo_path):
+				shutil.rmtree(repo_path)
 
-	except KeyboardInterrupt as e:
-		if os.path.exists(repo_path):
-			shutil.rmtree(repo_path)
+			raise e
 
-		raise e
+		if p.returncode != 0:
+			if os.path.exists(repo_path):
+				shutil.rmtree(repo_path)
 
-	if p.returncode != 0:
-		if os.path.exists(repo_path):
-			shutil.rmtree(repo_path)
+			raise Exception(p.stderr.decode())
 
-		raise Exception(p.stderr.decode())
-
-	yield RunParams(versions, repo_path, undo_renamed_vars)
-
-	shutil.rmtree(repo_path)
+		yield RunParams(versions, repo_path, undo_renamed_vars)
 
 
 @pytest.fixture(
