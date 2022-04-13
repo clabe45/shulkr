83c5b0693e1c1b1d550adcd42585c27939f3eff0
Caleb Sacks <16855387+clabe45@users.noreply.github.com>
2022-04-13 13:30:21 +0000

    chore: use keepachangelog package for version bumping

---
+++ scripts/bump.py
@@ -0,0 +1,34 @@
+import git
+import keepachangelog
+
+
+def current_version():
+	changes = keepachangelog.to_dict('CHANGELOG.md')
+	return sorted(changes.keys())[-1]
+
+
+def main():
+	old_version = current_version()
+
+	keepachangelog.release('CHANGELOG.md')
+
+	new_version = current_version()
+
+	with open('setup.py', 'r') as setuppy:
+		setuppy_code = setuppy.read()
+
+	new_setuppy_code = setuppy_code.replace(old_version, new_version)
+
+	with open('setup.py', 'w') as setuppy:
+		setuppy.write(new_setuppy_code)
+
+	repo = git.Repo()
+	commit_message = f'chore: release version {new_version}\n\nBump version {old_version} → {new_version}'
+	repo.git.commit('-m', commit_message, 'CHANGELOG.md', 'setup.py')
+	repo.git.tag('-a', '-m', f'version {new_version}', f'v{new_version}')
+
+	print(f'Bumped version {old_version} → {new_version}')
+
+
+if __name__ == '__main__':
+	main()
