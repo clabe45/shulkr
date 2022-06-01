5fcb3db6d4db48bf3a1cff73234966fe5bd74f1e
Caleb Sacks <16855387+clabe45@users.noreply.github.com>
2022-05-31 23:10:29 -0700

    test: add integration test for get_latest_generated_version

--- tests/integration/minecraft/test_version.py
+++ tests/integration/minecraft/test_version.py
@@ -3,6 +3,18 @@ import git
 from shulkr.minecraft.version import Version, get_latest_generated_version
 
 
+def test_get_latest_generated_version_with_repo_with_two_versions_returns_newer_version(repo: git.Repo):
+	# Add two tagged commits (Minecraft versions)
+	repo.git.commit(message='1.17', allow_empty=True)
+	repo.git.tag('1.17')
+
+	repo.git.commit(message='1.18', allow_empty=True)
+	repo.git.tag('1.18')
+
+	# The latest generated version from HEAD should be 1.18
+	assert get_latest_generated_version() == Version.of('1.18')
+
+
 def test_get_latest_generated_version_with_repo_with_two_versions_after_checking_out_older_version_returns_newer_version(repo: git.Repo):
 	# Add two tagged commits (Minecraft versions)
 	repo.git.commit(message='1.17', allow_empty=True)
